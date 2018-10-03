93a8fa3eb4df9e22ce0f4e52ed808805edf73a34
ritvikareddy18 <ritvikareddy18@ccs.neu.edu>
2018-10-03 13:52:16 -0400

    building bot

---
+++ test_flask.py
@@ -0,0 +1,22 @@
+# __author__ = ritvikareddy2
+# __date__ = 10/1/18
+
+# import pymessenger
+
+from flask import Flask, request
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def get_message():
+    # if request.method:
+    #     token_sent = request.args.get("hub.verify_token")
+    #     return verify_fb_token(token_sent)
+    # else:
+
+
+    return "Hello World, I am trying to be deployed"
+
+# $ export FLASK_APP = hello.py
+# $ flask run
